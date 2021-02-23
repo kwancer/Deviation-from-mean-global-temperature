6c454d8200bcbeadb46093d83340e179494a0d29
Krzysztof Wancerski <20wancerskikrzysztof@colchsfc.ac.uk>
2021-02-23 09:50:54 +0000

    finished the graph.

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
-async function getData(){
+
+
+async function chartIt() {
+  const data = await getData()
+  const ctx = document.getElementById('chart').getContext('2d');
+  
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: data.xs,
+      datasets: [{
+        fill: false,
+        label: 'Deviation from mean global temperature',
+        data: data.ys,
+        backgoundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(32,99,132,1)",
+          
+        
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            callback: function(value,index,values){
+              return value + "°"
+            }
+          }
+        }]
+      }
+    }
+  });
+
+}
+
+
+
+
+async function getData() {
+  const xs = []
+  const ys =[]
   const response = await fetch("test.csv");
   const data = await response.text();
-  
+
 
   const table = data.split("\n").slice(1)
   table.forEach(elt => {
     const row = elt.split(",")
     const year = row[0]
+    xs.push(year)
     const temp = row[1]
-    console.log(year, temp); 
+    ys.push(temp)
+    console.log(year, temp);
   })
-  
+  return {xs ,ys }
 }
 
-getData()
+
+chartIt()
